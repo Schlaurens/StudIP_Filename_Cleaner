829521909e8f231b9ce979f5887e9e8177667d56
Laurens Schiefelbein <laurens@uni-bremen.de>
2020-11-03 21:25:23 +0100

    now works with subdirectories

--- cleanFilenames.py
+++ cleanFilenames.py
@@ -4,5 +4,14 @@ import re
 if os.path.isfile("archive_filelist.csv"):
     os.remove("archive_filelist.csv")
 
-for filename in os.listdir("."):
-    os.rename(filename, re.sub('^[+[0-9]+]_', '', filename))
+filelist = []
+
+#get the path of every file in every directory and subdirectory
+for root, dirs, files in os.walk("."):
+    for file in files: 
+        filelist.append(os.path.join(root, file))
+        
+#rename the filenames
+for filename in filelist: 
+    #os.rename(filename, re.sub('^[+[0-9]+]_', '', os.path.basename(filename)))
+    os.rename(filename, os.path.join(os.path.dirname(filename), re.sub('^[+[0-9]+]_', '', os.path.basename(filename))))
