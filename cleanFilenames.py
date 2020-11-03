87793a488cb3d91b36187fb9160a8f6d52166a0b
Laurens Schiefelbein <laurens@uni-bremen.de>
2020-11-03 22:31:01 +0100

    can now delete 'archive_filelist.csv' in every sub

--- cleanFilenames.py
+++ cleanFilenames.py
@@ -1,14 +1,15 @@
 import os
 import re
 
-if os.path.isfile("archive_filelist.csv"):
-    os.remove("archive_filelist.csv")
-
-filelist = []
-
 #get the path of every file in every directory and subdirectory
 for root, dirs, files in os.walk("."):
     for file in files: 
+
+        if file == "archive_filelist.csv":
+            os.remove(os.path.join(root, file))
+            continue
+
         filename = os.path.join(root,file)
+        
         #rename the files
         os.rename(filename, os.path.join(root, re.sub('^[+[0-9]+]_', '', file)))
